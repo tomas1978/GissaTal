95cad28b93c0366ddfbebdf4a5b2d8ca380889bb
tomas1978 <tomas.nilsson78@gmail.com>
2019-09-30 19:02:09 +0200

    GUI finished

--- GissaTalForms/GissaTalForms/Form1.Designer.cs
+++ GissaTalForms/GissaTalForms/Form1.Designer.cs
@@ -30,36 +30,99 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.resetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guessCounterLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guessInput = new System.Windows.Forms.TextBox();
+            this.guessMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 151);
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(49, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Guess";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Gissa";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // resetButton
             // 
-            this.resetButton.Location = new System.Drawing.Point(57, 231);
+            this.resetButton.BackColor = System.Drawing.Color.Red;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(49, 264);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(90, 46);
             this.resetButton.TabIndex = 1;
-            this.resetButton.Text = "Reset";
-            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Text = "Nollställ";
+            this.resetButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Antal gissningar:";
+            // 
+            // guessCounterLabel
+            // 
+            this.guessCounterLabel.AutoSize = true;
+            this.guessCounterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessCounterLabel.Location = new System.Drawing.Point(161, 23);
+            this.guessCounterLabel.Name = "guessCounterLabel";
+            this.guessCounterLabel.Size = new System.Drawing.Size(18, 20);
+            this.guessCounterLabel.TabIndex = 3;
+            this.guessCounterLabel.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Din gissning";
+            // 
+            // guessInput
+            // 
+            this.guessInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessInput.Location = new System.Drawing.Point(112, 86);
+            this.guessInput.Name = "guessInput";
+            this.guessInput.Size = new System.Drawing.Size(61, 26);
+            this.guessInput.TabIndex = 5;
+            // 
+            // guessMessage
+            // 
+            this.guessMessage.AutoSize = true;
+            this.guessMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessMessage.Location = new System.Drawing.Point(16, 141);
+            this.guessMessage.Name = "guessMessage";
+            this.guessMessage.Size = new System.Drawing.Size(173, 20);
+            this.guessMessage.TabIndex = 6;
+            this.guessMessage.Text = "Du har inte gissat ännu";
             // 
             // guessButton
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(217, 289);
+            this.ClientSize = new System.Drawing.Size(219, 328);
+            this.Controls.Add(this.guessMessage);
+            this.Controls.Add(this.guessInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guessCounterLabel);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.button1);
             this.Name = "guessButton";
             this.Text = "Gissa Tal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +130,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label guessCounterLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox guessInput;
+        private System.Windows.Forms.Label guessMessage;
     }
 }
 
--- GissaTalForms/GissaTalForms/Form1.cs
+++ GissaTalForms/GissaTalForms/Form1.cs
@@ -16,5 +16,6 @@ namespace GissaTalForms
         {
             InitializeComponent();
         }
+
     }
 }
